72375d4919977eba394bb270de9c2041745a65d3
Abidijihed <abidij55@gmail.com>
2021-04-17 14:58:06 +0200

    save

--- database/models/products.js
+++ database/models/products.js
@@ -3,7 +3,9 @@ module.exports={
     createProduct: (req,res)=>{   
         const query=`INSERT INTO products(name,information,category,oldprice,newprice,type,quantity,image,status,promotion) VALUES('${req.body.name}','${req.body.information}','${req.body.category}','${req.body.oldprice}','${req.body.newprice}','${req.body.type}','${req.body.quantity}','${req.body.image}','${req.body.status}','${req.body.promotion}')`
         connection.query(query,(err,results)=>{
+           
             err ? res.status(500).send(err) : res.status(201).send(results)
+
         })
    
     },
