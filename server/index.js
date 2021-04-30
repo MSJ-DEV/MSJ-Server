01a56771e4c8bc8d81d3a2529807e32593634f58
rmadi <rmadi.med1@gmail.com>
2021-04-30 02:32:00 +0200

    start with node mail

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const nodemailer = require("nodemailer");
+
 
 var cors = require('cors')
 const db = require('../database/index.js')
@@ -45,6 +47,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // port
 app.set("port", 3333);
 
+// ****************************** NODE MAIL **************************
+let transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: testAccount.user, // generated ethereal user
+    pass: testAccount.pass, // generated ethereal password
+  },
+});
+
+// send mail with defined transport object
+let info = await transporter.sendMail({
+  from: '"Fred Foo 👻" <foo@example.com>', // sender address
+  to: "bar@example.com, baz@example.com", // list of receivers
+  subject: "Hello ✔", // Subject line
+  text: "Hello world?", // plain text body
+  html: "<b>Hello world?</b>", // html body
+});
+
+console.log("Message sent: %s", info.messageId);
+// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+// Preview only available when sending through an Ethereal account
+console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+
+
+// ****************************** NODE MAIL **************************
 
 
 app.get("/", function (req, res) {
