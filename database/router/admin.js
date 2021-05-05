2767b5a5b6b95735e4f3a41b0e6c32b26079e811
rmadi <rmadi.med1@gmail.com>
2021-05-05 19:51:49 +0200

    incommen change

--- database/router/admin.js
+++ database/router/admin.js
@@ -1,6 +1,6 @@
 const router= require("express").Router()
 const models=require('../models/admin.js')
 router.post('/',models.createAdmin)
-// router.get('/',models.getoneAdmin)
+router.get('/',models.getoneAdmin)
 router.get('/api/getall',models.getAll)
 module.exports={routerAdmin:router}
